ea02b10c1e7ebd89eee47256de05cc017b826b3a
Juan Bordagorry <juan.bordagorry@gmail.com>
2021-08-27 08:44:47 -0300

    Add classes

---
+++ Threads/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clase26_8 clase = new Clase26_8();
+
+            clase.Ejercicio7();
+        }
+        
+    }
+}
